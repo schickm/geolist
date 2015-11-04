e015a33420f35463fa89826bfd2096ad7a2b7e5a
Matt Schick <matt@schickm.com>
2015-11-04 13:11:12 -0800

    start using eslint

--- client/templates/list/list-detail.js
+++ client/templates/list/list-detail.js
@@ -1,64 +1,70 @@
-Number.prototype.toRad = function () {
-    return this * (Math.PI / 180);
-};
+// boring
+function toRad(num) {
+    return num * (Math.PI / 180);
+}
 
 function calcDistance(lat1, lon1, lat2, lon2) {
-    var R = 6371; // km
-    var dLat = (lat2-lat1).toRad();
-    var dLon = (lon2-lon1).toRad();
-    lat1 = lat1.toRad();
-    lat2 = lat2.toRad();
+    let R = 6371, // km
+        dLat = toRad(lat2 - lat1),
+        dLon = toRad(lon2 - lon1),
+        dLat1 = toRad(lat1),
+        dLat2 = toRad(lat2),
+        a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.sin(dLon / 2) * Math.sin(dLon / 2) *
+            Math.cos(dLat1) * Math.cos(dLat2),
+        c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-            Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-    
     return R * c;
 }
 
 Template.listDetail.helpers({
-    itemsSorted: function() {        
-        var position = Geolocation.currentLocation(),
+    itemsSorted() {
+        let position = Geolocation.currentLocation(),
             query = {listId: Router.current().params._id};
 
         if (! Session.get('showCompletedItems')) {
             query.done = false;
         }
-        var itemList = Items.find(query).fetch();
+        let itemList = Items.find(query).fetch();
 
         if (position) {
-            itemList = _.sortBy(itemList, function(item) {
+            itemList = _.sortBy(itemList, item => {
                 if (item.lat && item.lng) {
-                    return calcDistance(position.coords.latitude, position.coords.longitude, item.lat, item.lng);
+                    return calcDistance(position.coords.latitude,
+                                        position.coords.longitude,
+                                        item.lat, item.lng);
                 }
             });
         }
-        
+
         return _.sortBy(itemList, 'done');
     },
 
-    showCompletedItems: function() {
+    showCompletedItems() {
         return Session.get('showCompletedItems');
     },
 
-    canShareList: function() {
-    	if (Lists.findOne({_id: Router.current().params._id, userId: Meteor.userId()})) {
-    		return true;
-    	} else {
-    		return false;
-    	}
+    canShareList() {
+        if (Lists.findOne({
+            _id: Router.current().params._id,
+            userId: Meteor.userId(),
+        })) {
+            return true;
+        }
+
+        return false;
     },
 
-    currentPositionString: function() {
-        var position = Geolocation.currentLocation();
+    currentPositionString() {
+        let position = Geolocation.currentLocation();
         if (position) {
             return `${position.coords.latitude}N ${position.coords.longitude}E`;
         }
-    }
+    },
 });
 
 Template.listDetail.events({
-    'submit form': function(event) {
+    'submit form': function submitFormHandler(event) {
         event.preventDefault();
         var input = event.target.item,
             itemLabel = input.value,
@@ -69,16 +75,16 @@ Template.listDetail.events({
                 quantity: 1,
                 lat: null,
                 lng: null,
-                done: false
+                done: false,
             };
 
-        Meteor.call('addListItem', listItemDoc, function() {
+        Meteor.call('addListItem', listItemDoc, () => {
             // blank out the input
             input.value = '';
         });
     },
 
-    'click .completed-toggle': function() {
+    'click .completed-toggle': function clickCompletedToggleHandler() {
         Session.set('showCompletedItems', ! Session.get('showCompletedItems'));
-    }
+    },
 });
